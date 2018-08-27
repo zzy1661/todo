d31892537710b44d5c32f95c0b411f1c6d9c0b93
zzy1661 <1556597051@qq.com>
2018-08-27 13:39:13 +0800

    修改路由

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
+import React, {
+	Component
+} from 'react';
 import './App.css';
-import { BrowserRouter 	as Router, Route, Link, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch, Redirect } from 'react-router-dom';
 
 import Login from './pages/login';
 import Index from './pages/index';
 
 class App extends Component {
 	constructor(props) {
-    super(props);
-   
-  }
-	
-  render() {
-    return (
-      <Router>
+		super(props);
+
+	}
+
+	render() {
+		return(
+			<Router>
 				<Switch>
-        	<Route exact path="/" render={ ()=> ( <Redirect to="/login" />)} />
+	        		<Route exact path="/" render={ ()=> ( <Redirect to="/login" />)} />
 					<Route path="/login" component={Login} />
 					<Route path="/index" component={Index} />
 					
 				</Switch>
-      </Router>
-    );
-  }
+	      </Router>
+		);
+	}
 }
 
 export default App;
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,31 +1,41 @@
-import React, { Component } from 'react';
+import React, {
+	Component
+} from 'react';
 import { Form, Input, Button } from 'antd';
+import {BrowserHistory } from 'react-router-dom';
 import './index.css';
 const FormItem = Form.Item;
 class Login extends Component {
 	constructor(props) {
-    super(props);
-    this.state = {imgBgStyle: {
-    	display: 'none'
-    }};
-  }
+		super(props);
+		this.state = {
+			imgBgStyle: {
+				display: 'none'
+			}
+		};
+	}
+	login() {
+//		BrowserHistory.push('/index')
+	}
 	componentWillMount() {
-      const img = new Image()
-    	img.src = 'https://placeimg.com/1920/1080/any';
-    	img.onload = () => {
-    		this.setState({imgBgStyle: {
-    			display: 'block',
-    			height: '100%',
+		const img = new Image()
+		img.src = 'https://placeimg.com/1920/1080/any';
+		img.onload = () => {
+			this.setState({
+				imgBgStyle: {
+					display: 'block',
+					height: '100%',
 					background: `url(${img.src}) 50% 50%/cover no-repeat`
-    		}});
-    	}
-  }
-	 componentDidMount() {
-    
-  }
-  render() {
-    return (
-      <div className="login-wrapper" >
+				}
+			});
+		}
+	}
+	componentDidMount() {
+
+	}
+	render() {
+		return(
+			<div className="login-wrapper" >
         <div className="login-bg-img" style={this.state.imgBgStyle}></div>
         <div className="login-panel">
         	<h1 className="login-logo">TODO</h1>
@@ -37,13 +47,13 @@ class Login extends Component {
         			<Input placeholder="密码" type="password"/>
         		</FormItem>
         		<FormItem>
-        			<Button type="primary" block>Start!</Button>
+        			<Button type="primary" block onClick={this.login}>Start!</Button>
         		</FormItem>
         	</Form>
         </div>
       </div>
-    );
-  }
+		);
+	}
 }
 
 export default Login;
