016b7b8908a41c12f27cd5dafcba06c74976fcdc
zzy1661 <1556597051@qq.com>
2018-09-01 20:38:28 +0800

    add time组建

---
+++ src/components/timer/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Utils from '../../lib/utils';
+import './timer.css';
+
+class Timer extends Component {
+
+    state = {
+        date: new Date() 
+    }
+
+    componentDidMount () {
+        this.timerId = setInterval(() => this.updatime(),1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    updatime () {
+        this.setState({
+            date: new Date()
+        })
+    }
+
+    render () {
+        return (
+            <span>{Utils.dateFormat(this.state.date,'yyyy-MM-dd HH:mm:ss')} </span>
+        )
+    }
+}
+
+export default Timer;
