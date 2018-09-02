29d77945a28c1cc3366b6f867296220c4721466d
zzy1661 <1556597051@qq.com>
2018-09-02 19:11:57 +0800

    add 任务列表请求和渲染

--- src/pages/allPlan/index.js
+++ src/pages/allPlan/index.js
@@ -1,63 +1,63 @@
 import React, { Component } from 'react';
-import { Table, Divider, Tag } from 'antd';
-const columns = [{
-    title: '创建时间',
-    dataIndex: 'name',
-    key: 'name',
-    render: text => <a href="javascript:;">{text}</a>,
-  }, {
-    title: '任务名称',
-    dataIndex: 'age',
-    key: 'age',
-  }, {
-    title: '任务状态',
-    dataIndex: 'address',
-    key: 'address',
-  }, {
-    title: '详情',
-    key: 'tags',
-    dataIndex: 'tags',
-    render: tags => (
-      <span>
-        {tags.map(tag => <Tag color="blue" key={tag}>{tag}</Tag>)}
-      </span>
-    ),
-  }, {
-    title: '操作',
-    key: 'action',
-    render: (text, record) => (
-      <span>
-        <a href="javascript:;">Invite {record.name}</a>
-        <Divider type="vertical" />
-        <a href="javascript:;">Delete</a>
-      </span>
-    ),
-  }];
+import { Table, Button } from 'antd';
   
-  const data = [{
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-    tags: ['nice', 'developer'],
-  }, {
-    key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-    tags: ['loser'],
-  }, {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-    tags: ['cool', 'teacher'],
-  }];
 class AllPlan extends Component {
     
+    state = {
+      columns: [{
+        title: '创建时间',
+        dataIndex: 'creatime',
+        key: 'creatime',
+      }, {
+        title: '任务名称',
+        dataIndex: 'name',
+        key: 'name',
+      }, {
+        title: '任务状态',
+        dataIndex: 'status',
+        key: 'status',
+        render: status => {
+          console.log(status);
+          var statusList = ["已完成","进行中", "超时", "封存"];
+          return (
+            <span>{statusList[status]}</span>
+          )
+        }
+      }, {
+        title: '详情',
+        key: 'describe',
+        dataIndex: 'describe',
+      }, {
+        title: '操作',
+        key: 'action',
+        render: (text, record) => (
+          <span>
+            <Button type="primary" className="m-2">完成</Button>
+            <Button type="primary" className="m-2">封存</Button>
+            <Button type="primary" className="m-2">删除</Button>
+          </span>
+        ),
+      }],
+      data: null
+    }
+
+    componentWillMount() {
+      fetch('https://easy-mock.com/mock/5b8baba761840c7b4033654b/todo/task',{
+        method: 'GET'
+      }).then( res => res.json())
+      .then( data => {
+        if(data.code === 1) {
+          this.setState({
+            data: data.data
+          })
+        }
+      })
+      
+    }
+
     render () {
         return (
-            <Table columns={columns} dataSource={data} />
+            <Table rowKey="id" columns={this.state.columns} dataSource={this.state.data} />
         )
     }
 }
--- src/pages/todoPlan/index.js
+++ src/pages/todoPlan/index.js
@@ -1,11 +1,63 @@
 import React, { Component } from 'react';
-
+import { Table, Button } from 'antd';
 class TodoPlan extends Component {
     
-    render () {
-        return (
-            <div>TodoPlan</div>
-        )
-    }
+    state = {
+        columns: [{
+          title: '创建时间',
+          dataIndex: 'creatime',
+          key: 'creatime',
+        }, {
+          title: '任务名称',
+          dataIndex: 'name',
+          key: 'name',
+        }, {
+          title: '任务状态',
+          dataIndex: 'status',
+          key: 'status',
+          render: status => {
+            console.log(status);
+            var statusList = ["已完成","进行中", "超时", "封存"];
+            return (
+              <span>{statusList[status]}</span>
+            )
+          }
+        }, {
+          title: '详情',
+          key: 'describe',
+          dataIndex: 'describe',
+        }, {
+          title: '操作',
+          key: 'action',
+          render: (text, record) => (
+            <span>
+              <Button type="primary" className="m-2">完成</Button>
+              <Button type="primary" className="m-2">封存</Button>
+              <Button type="primary" className="m-2">删除</Button>
+            </span>
+          ),
+        }],
+        data: null
+      }
+  
+      componentWillMount() {
+        fetch('https://easy-mock.com/mock/5b8baba761840c7b4033654b/todo/task',{
+          method: 'GET'
+        }).then( res => res.json())
+        .then( data => {
+          if(data.code === 1) {
+            this.setState({
+              data: data.data
+            })
+          }
+        })
+        
+      }
+  
+      render () {
+          return (
+              <Table rowKey="id" columns={this.state.columns} dataSource={this.state.data} />
+          )
+      }
 }
 export default TodoPlan;
