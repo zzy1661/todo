7111df4c626e329cc74f4cc984678739144f05e5
zzy1661 <1556597051@qq.com>
2018-09-04 02:04:25 +0800

    add 创建任务页面

---
+++ src/pages/createTask/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class CreateTask extends Component {
+    render () {
+        return (
+            <div>创建任务</div>
+        )
+    }
+}
+export default CreateTask;
