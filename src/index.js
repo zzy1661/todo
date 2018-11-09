0a425501e3f8eb4a4960aa099365d0de6a2754b8
zzy1661 <1556597051@qq.com>
2018-11-10 05:10:37 +0800

    add 国际化设置

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { LocaleProvider } from 'antd';
+
+import zh_CN from 'antd/lib/locale-provider/zh_CN';
+
+import 'moment/locale/zh-cn';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render( <LocaleProvider locale={zh_CN}><App /></LocaleProvider>, document.getElementById('root'));
 registerServiceWorker();
