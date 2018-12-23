0d6f9ff7994505c271b856419b1809fda7fbed03
zzy1661 <1556597051@qq.com>
2018-12-23 22:10:59 +0800

    登录页login改造

--- src/App.js
+++ src/App.js
@@ -4,25 +4,47 @@ import React, {
 import './App.css';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
-import Login from './pages/login';
-import Index from './pages/index';
-import { hot } from 'react-hot-loader';
+import Login from './containers/Login';
+import Index from './containers/index';
+// import { hot } from 'react-hot-loader';
+
+
+/* import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const userReducer = (state, action) => {
+	if (!state) return {
+		username: sessionStorage.getItem('username'),
+		userToken: sessionStorage.getItem('userToken')
+	}
+	switch (action.type) {
+		case 'login':
+			return { ...state, username: action.username, userToken: action.userToken }
+		case 'logout':
+			return { ...state, username: '', userToken: '' }
+		default:
+			return state
+	}
+}
+const store = createStore(userReducer) */
+
 class App extends Component {
 
 	render() {
-		return(
+		return (
 			<Router>
 				<Switch>
 					<Route path="/login" component={Login} />
 					<Redirect exact from="/" to="/general" />
 					<Route path="/general" component={Index} />
-					<Route path="/workbench" component={ Index }></Route>
-					<Route path="/statistics" component={ Index }></Route>
+					<Route path="/workbench" component={Index}></Route>
+					<Route path="/statistics" component={Index}></Route>
 					<Redirect to="/login" />
 				</Switch>
-	      </Router>
+			</Router>
 		);
 	}
 }
 
-export default hot(module)(App)
+// export default <Provider store={store}><App /></Provider>;
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import { LocaleProvider } from 'antd';
-
-import zh_CN from 'antd/lib/locale-provider/zh_CN';
-
-import 'moment/locale/zh-cn';
 import ReactDOM from 'react-dom';
+
+import { LocaleProvider } from 'antd';
+import zh_CN from 'antd/lib/locale-provider/zh_CN';
+import 'moment/locale/zh-cn';
+
+
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render( <LocaleProvider locale={zh_CN}><App /></LocaleProvider>, document.getElementById('root'));
-registerServiceWorker();
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const userReducer = (state, action) => {
+	if (!state) return {
+		username: sessionStorage.getItem('username'),
+		userToken: sessionStorage.getItem('userToken')
+	}
+	switch (action.type) {
+		case 'login':
+			return { ...state, username: action.username, userToken: action.userToken }
+		case 'logout':
+			return { ...state, username: '', userToken: '' }
+		default:
+			return state
+	}
+}
+const store = createStore(userReducer)
+ReactDOM.render( <LocaleProvider locale={zh_CN}><Provider store={store}><App /></Provider></LocaleProvider>, document.getElementById('root'));
+// registerServiceWorker();
