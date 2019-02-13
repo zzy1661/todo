f21b76776d5b8314b584fdea4a582b6784fba1a1
周振宇 <zhenyu.zhou@expread.io>
2019-02-13 20:47:22 +0800

    修改任务概览显示内容

--- src/components/allPlan/index.js
+++ src/components/allPlan/index.js
@@ -1,11 +1,6 @@
-import React, {
-    Component
-} from 'react';
-import PropTypes from 'prop-types';
-import {
-    Table,
-    Button
-} from 'antd';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Table, Button } from "antd";
 
 class AllPlan extends Component {
     static propTypes = {
@@ -13,57 +8,73 @@ class AllPlan extends Component {
         userToken: PropTypes.string,
         removeUser: PropTypes.func,
         tasks: PropTypes.array,
-        getTasks: PropTypes.func,
-    }
+        getTasks: PropTypes.func
+    };
     state = {
-        columns: [{
-            title: '创建时间',
-            dataIndex: 'creatime',
-            key: 'creatime',
-        }, {
-            title: '任务名称',
-            dataIndex: 'name',
-            key: 'name',
-        }, {
-            title: '开始时间',
-            dataIndex: 'startime',
-            key: 'startime',
-        }, {
-            title: '结束时间',
-            dataIndex: 'endtime',
-            key: 'endtime',
-        }, {
-            title: '任务状态',
-            dataIndex: 'status',
-            key: 'status',
-            render: status => {
-                var statusList = ['进行中', '已完成', '暂停', '终止', '超时', '未开始'];
-                return ( <
-                    span > {
-                        statusList[status]
-                    } < /span>
+        columns: [
+            {
+                title: "创建时间",
+                dataIndex: "creatime",
+                key: "creatime"
+            },
+            {
+                title: "任务名称",
+                dataIndex: "name",
+                key: "name"
+            },
+            {
+                title: "开始时间",
+                dataIndex: "startime",
+                key: "startime"
+            },
+            {
+                title: "结束时间",
+                dataIndex: "endtime",
+                key: "endtime"
+            },
+            {
+                title: "任务状态",
+                dataIndex: "status",
+                key: "status",
+                render: status => {
+                    var statusList = [
+                        "进行中",
+                        "已完成",
+                        "暂停",
+                        "终止",
+                        "超时",
+                        "未开始"
+                    ];
+                    return <span> {statusList[status]} </span>;
+                }
+            },
+            {
+                title: "详情",
+                key: "des",
+                dataIndex: "des"
+            },
+            {
+                title: "操作",
+                key: "action",
+                render: (text, record) => (
+                    <span>
+                        <Button type="primary" className="m-2">
+                            {" "}
+                            完成{" "}
+                        </Button>{" "}
+                        <Button type="primary" className="m-2">
+                            {" "}
+                            封存{" "}
+                        </Button>{" "}
+                        <Button type="primary" className="m-2">
+                            {" "}
+                            删除{" "}
+                        </Button>{" "}
+                    </span>
                 )
             }
-        }, {
-            title: '详情',
-            key: 'des',
-            dataIndex: 'des',
-        }, {
-            title: '操作',
-            key: 'action',
-            render: (text, record) => ( <
-                span >
-                <
-                Button type = "primary"
-                className = "m-2" > 完成 < /Button> <
-                Button type = "primary"
-                className = "m-2" > 封存 < /Button> <
-                Button type = "primary"
-                className = "m-2" > 删除 < /Button> <
-                /span>
-            ),
-        }],
-    }
+        ]
+    };
 
     componentDidMount() {
         if (!this.props.username || !this.props.userToken) {
@@ -74,16 +85,13 @@ class AllPlan extends Component {
     }
 
     render() {
-        return ( <
-            Table rowKey = "id"
-            columns = {
-                this.state.columns
-            }
-            dataSource = {
-                this.props.tasks
-            }
+        return (
+            <Table
+                rowKey="id"
+                columns={this.state.columns}
+                dataSource={this.props.tasks}
             />
-        )
+        );
     }
 }
 export default AllPlan;
--- src/components/todoPlan/index.js
+++ src/components/todoPlan/index.js
@@ -1,63 +1,97 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Table, Button } from 'antd';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Table, Button } from "antd";
 
 class TodoPlan extends Component {
-  static propTypes = {
-    username: PropTypes.string,
-    userToken: PropTypes.string,
-    removeUser: PropTypes.func,
-    tasks: PropTypes.array,
-    getTasks: PropTypes.func,
-  }
-  state = {
-    columns: [{
-      title: '创建时间',
-      dataIndex: 'startime',
-      key: 'startime',
-    }, {
-      title: '任务名称',
-      dataIndex: 'name',
-      key: 'name',
-    }, {
-      title: '任务状态',
-      dataIndex: 'status',
-      key: 'status',
-      render: status => {
-        var statusList = ['进行中', '已完成', '暂停', '终止', '超时', '未开始'];
-        return (
-          <span>{statusList[status]}</span>
-        )
-      }
-    }, {
-      title: '详情',
-      key: 'des',
-      dataIndex: 'des',
-    }, {
-      title: '操作',
-      key: 'action',
-      render: (text, record) => (
-        <span>
-          <Button type="primary" className="m-2">完成</Button>
-          <Button type="primary" className="m-2">封存</Button>
-          <Button type="primary" className="m-2">删除</Button>
-        </span>
-      ),
-    }],    
-  }
+    static propTypes = {
+        username: PropTypes.string,
+        userToken: PropTypes.string,
+        removeUser: PropTypes.func,
+        tasks: PropTypes.array,
+        getTasks: PropTypes.func
+    };
+    state = {
+        columns: [
+            {
+                title: "创建时间",
+                dataIndex: "startime",
+                key: "startime"
+            },
+            {
+                title: "任务名称",
+                dataIndex: "name",
+                key: "name"
+            },
+            {
+                title: "开始时间",
+                dataIndex: "startime",
+                key: "startime"
+            },
+            {
+                title: "结束时间",
+                dataIndex: "endtime",
+                key: "endtime"
+            },
+            {
+                title: "任务状态",
+                dataIndex: "status",
+                key: "status",
+                render: status => {
+                    var statusList = [
+                        "进行中",
+                        "已完成",
+                        "暂停",
+                        "终止",
+                        "超时",
+                        "未开始"
+                    ];
+                    return <span>{statusList[status]}</span>;
+                }
+            },
+            {
+                title: "详情",
+                key: "des",
+                dataIndex: "des"
+            },
+            {
+                title: "操作",
+                key: "action",
+                render: (text, record) => (
+                    <span>
+                        <Button type="primary" className="m-2">
+                            完成
+                        </Button>
+                        <Button type="primary" className="m-2">
+                            封存
+                        </Button>
+                        <Button type="primary" className="m-2">
+                            删除
+                        </Button>
+                    </span>
+                )
+            }
+        ]
+    };
 
-  componentDidMount() {   
-    if (!this.props.username || !this.props.userToken) {
-      this.props.removeUser();
-      return;
+    componentDidMount() {
+        if (!this.props.username || !this.props.userToken) {
+            this.props.removeUser();
+            return;
+        }
+        this.props.getTasks(this.props.userToken);
     }
-    this.props.getTasks(this.props.userToken);   
-  }
 
-  render() {
-    return (
-      <Table rowKey="id" columns={this.state.columns} dataSource={this.props.tasks&&this.props.tasks.filter(task=>task.status===0)} />
-    )
-  }
+    render() {
+        return (
+            <Table
+                rowKey="id"
+                columns={this.state.columns}
+                dataSource={
+                    this.props.tasks &&
+                    this.props.tasks.filter(task => task.status === 0)
+                }
+            />
+        );
+    }
 }
 export default TodoPlan;
